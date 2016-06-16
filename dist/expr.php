b830040f27455e026190a36b389e60e0824b5007
makingoff <mail@makingoff.name>
2016-06-16 20:38:44 +0300

    Removes dist from repo

--- dist/expr.php
+++
@@ -1,1 +0,0 @@
-<?php echo $b + $c[$d]["str"][$foo($bar, $zoo[2])] * 2 / ($func() - $Math["calc"](1, 2)) * -1; ?>
--- dist/if.php
+++
@@ -1,7 +0,0 @@
-<?php if ($a == $b) { ?>
-  <?php $a = $b + $c; ?>
-<?php } elseif ($c != $d) { ?>
-  <?php $e = $f * $d; ?>
-<?php } else { ?>
-  <?php echo $i - $g; ?>
-<?php } ?>
