4a1cf2b84a6d5594c76bc405e4d25accee3eb27d
Aleksei Chikin <mail@makingoff.name>
2018-05-13 00:06:35 +0300

    Adds id field to script node

--- tokens/script.js
+++ tokens/script.js
@@ -1,9 +1,12 @@
+let id = 0
+
 function Script (attrs, body, line, column) {
 	this.type = 'script'
 	this.attrs = attrs
 	this.body = body
 	this.line = line
 	this.column = column
+	this.id = id++
 }
 
 module.exports = Script
